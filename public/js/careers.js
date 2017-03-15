a2ac1f9fedcf9573791cb3f486af8652118d53e0
Daniel Kim <danielkimlaws@gmail.com>
2017-03-15 18:35:40 +0900

    add

--- public/js/careers.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    var bgimage = new Image();      
-    bgimage.src="../public/images/about.jpg";       
-    $(bgimage).load(function(){
-  		$('.about-hero').addClass('visible');                  
-    });
-});
